40c313e30bc6eb39c5b3859de58a0d3cb4c74d48
Yash Raj Bharti <43868318+yashrajbharti@users.noreply.github.com>
2021-10-18 19:47:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,11 +34,7 @@ tl
   //   .to(img6, 1, {alpha: 1},1)
   //   .to(img5,0.9,{alpha: 0},1.1)
   // tl
-
   //   .to(img7, 1, {alpha: 1},1)
-
-  //   .to(img7, 1, {alpha: 1},1) // this one 
-
   //   .to(img6,0.9,{alpha: 0},1.1)
 
 	tl
@@ -80,7 +76,13 @@ var img3 = PIXI.Sprite.fromImage('l.jpg');
 	img3.position.y = 50;
 	img3.alpha = 0;
 	container.addChild(img3);
-
+// var img4 = PIXI.Sprite.fromImage('a.jpg');
+// 	img4.width = 650;
+// 	img4.height = 500;
+// 	img4.position.x = 75;
+// 	img4.position.y = 50;
+// 	img4.alpha = 0;
+// 	container.addChild(img4);
 // var img5 = PIXI.Sprite.fromImage('b.jpg');
 // 	img5.width = 650;
 // 	img5.height = 500;
@@ -88,22 +90,6 @@ var img3 = PIXI.Sprite.fromImage('l.jpg');
 // 	img5.position.y = 50;
 // 	img5.alpha = 0;
 // 	container.addChild(img5);
-
-var img4 = PIXI.Sprite.fromImage('a.jpg');
-	img4.width = 650;
-	img4.height = 500;
-	img4.position.x = 75;
-	img4.position.y = 50;
-	img4.alpha = 0;
-	container.addChild(img4);
-var img5 = PIXI.Sprite.fromImage('b.jpg');
-	img5.width = 650;
-	img5.height = 500;
-	img5.position.x = 75;
-	img5.position.y = 50;
-	img5.alpha = 0;
-	container.addChild(img5);
-
 // var img6 = PIXI.Sprite.fromImage('c.jpg');
 //   img6.width = 650;
 //   img6.height = 500;
